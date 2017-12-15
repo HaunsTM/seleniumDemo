00cc12bfc010329451709d66016ff918e6e68abb
BAL <bal@svep.se>
2017-12-15 11:47:41 +0100

    Completed all demo tests

--- simpleStaticHTMLPage.Tests/UnitTests.cs
+++ simpleStaticHTMLPage.Tests/UnitTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace simpleStaticHTMLPage.Tests
 {
@@ -10,8 +12,9 @@ namespace simpleStaticHTMLPage.Tests
     {
         IWebDriver _driver = null;
         IJavaScriptExecutor _javaScriptExecutor = null;
-
         private const string SIMPLE_STATIC_LOCAL_HTML_PAGE = "http://localhost:59714/index.html";
+
+        private TimeSpan maximumTimeToWaitForUIToFulfill = TimeSpan.FromSeconds(10);
 
         [SetUp]
         public void Initialize()
@@ -75,7 +78,7 @@ namespace simpleStaticHTMLPage.Tests
         public void T__Cant_Click_On_Multiplicate_Button_With_Invalid_Input()
         {
             //are we allowed to perform calculation, i.e. hit the MULTIPLY-button, when we have wrong input? 
-            bool isDisabled = false;
+            bool isDisabled = false; //just a dummy value, not meaning anything special
 
             //preparations: add some invalid input to input field
             var input = _driver.FindElement(By.XPath("html/body/div/div/main/div/form/div/input[@id ='txtNumber']"));
@@ -100,7 +103,39 @@ namespace simpleStaticHTMLPage.Tests
         {
             //do we get a correct result when we try to calculate the multiplication?
 
-            //this one is tricky... :-)
+            //preparations: add some valid input to input field
+            var input = _driver.FindElement(By.XPath("html/body/div/div/main/div/form/div/input[@id ='txtNumber']"));
+            const string THIRTYTWO = "32";
+
+            //type "32" in the text box
+            input.Clear(); // clear input field
+            input.SendKeys(THIRTYTWO);   // add THIRTYTWO to input field
+            
+            //click on MULTIPLICATE-button
+            var btnMultiplicate = _driver.FindElement(By.Id("btnMultiplicate"));
+            btnMultiplicate.Click();
+
+            //is the resulting dialog visible?
+            var dialog = _driver.FindElement(By.XPath("/html/body/div/div/main/div/dialog[@class='mdl-dialog']"));
+            var dialogIsVisible = dialog.Displayed && dialog.Enabled;
+            
+            Assert.AreEqual(expected: true, actual: dialogIsVisible, message: "The result dialog should be visible");
+
+            //lets get the result
+            var resultParagraph = _driver.FindElement(By.XPath("/html/body/div/div/main/div/dialog[@class='mdl-dialog']/div/div/p"));
+            var entireResultText = resultParagraph.Text;
+
+            //analyze text
+            var regex = new Regex($@"The multiplication, {THIRTYTWO} · 5, resulted in (?<result>\d+)");
+            var match = regex.Match(entireResultText);
+
+            //does the result text look like it should?
+            var patternIsRecognized = match.Success;
+            Assert.AreEqual(expected: true, actual: patternIsRecognized, message: "The result text should follow a predefined pattern.");
+
+            //do we have a correct result?
+            var resultValue = int.Parse(match.Groups["result"].Value);
+            Assert.AreEqual(expected: 5 * int.Parse(THIRTYTWO), actual: resultValue, message: "The result should be 5 · 30, i.e. 160.");
 
         }
 
@@ -108,9 +143,40 @@ namespace simpleStaticHTMLPage.Tests
         public void T__Input_IsClearedAfter_We_Have_Agreed_On_The_Result()
         {
             //is the input field cleared when we close
+            var waitForUI = 5 * 1000;
 
-            //this one is tricky... :-)
+            //preparations: add some valid input to input field
+            var input = _driver.FindElement(By.XPath("html/body/div/div/main/div/form/div/input[@id ='txtNumber']"));
+            const string THIRTYTWO = "32";
 
+            //type "32" in the text box
+            input.Clear();
+            input.SendKeys(THIRTYTWO);
+
+            System.Threading.Thread.Sleep(waitForUI);
+
+            //did we get anything in our input?
+            var inputText = input.GetAttribute("value");
+            Assert.AreEqual(expected: THIRTYTWO, actual: inputText, message: "The test should have typed 32 in input field");
+
+            //click on MULTIPLICATE-button
+            var btnMultiplicate = _driver.FindElement(By.Id("btnMultiplicate"));
+            btnMultiplicate.Click();
+
+            //is the resulting dialog visible?
+            var dialog = _driver.FindElement(By.XPath("/html/body/div/div/main/div/dialog[@class='mdl-dialog']"));
+            var dialogIsVisible = dialog.Displayed && dialog.Enabled;
+            Assert.AreEqual(expected: true, actual: dialogIsVisible, message: "The result dialog should be visible");
+
+            //close dialog 
+            var buttonAgree = _driver.FindElement(By.XPath("/html/body/div/div/main/div/dialog[@class='mdl-dialog']/div/button[contains(., 'Agree')]"));
+            buttonAgree.Click();
+
+            System.Threading.Thread.Sleep(waitForUI);
+
+            //did we have anything in our input now?
+            inputText = input.GetAttribute("value");
+            Assert.AreEqual(expected: String.Empty, actual: inputText, message: "The input field should be empty!");
         }
 
 
